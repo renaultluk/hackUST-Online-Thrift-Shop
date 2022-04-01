271aca097c95e05f44993507feb01c0790b0bac2
renaultluk <ren66.rl@gmail.com>
2022-04-01 16:16:23 +0800

    create successful order page

---
+++ main/components/FullHeightPage.js
@@ -0,0 +1,16 @@
+const FullHeightPage = ({ children }) => (
+    <div>
+      {children}
+      <style global jsx>{`
+        html,
+        body,
+        body > div:first-child,
+        div#__next,
+        div#__next > div {
+          height: 100%;
+        }
+      `}</style>
+    </div>
+)
+
+export default FullHeightPage
