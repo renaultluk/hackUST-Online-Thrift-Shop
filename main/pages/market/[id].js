fd6cacdc05b1790b56bffaad56a51b3ac408532b
renaultluk <ren66.rl@gmail.com>
2022-04-02 08:57:49 +0800

    put product info page into static paths template

--- main/pages/market/[id].js
+++ main/pages/market/[id].js
@@ -1,12 +1,72 @@
+import { useState } from "react"
+import { Container, Button, Carousel } from "react-bootstrap"
+import Image from 'next/image'
+
+import FullHeightPage from "../../components/FullHeightPage"
+import ProductTag from "../../components/ProductTag"
+
+import styles from "../../styles/product-info.module.css"
+
 export const getStaticPaths = async () => {
+    const products = await fetch("http://localhost:3000/api/products").then(res => res.json());
+    const paths = products.map(product => ({
+        params: {
+            id: product.id.toString()
+        }
+    }));
+    return {
+        paths,
+        fallback: false
+    };
+};
+
+export const getStaticProps = async(context) => {
+    const productId = context.params.id;
+    const product = await fetch(`http://localhost:3000/api/products/${productId}`).then(res => res.json());
+    return {
+        props: {
+            product
+        }
+    };
+}
+
+const ProductInfo = ({ product }) => {
+    // const [product, setProduct] = useState({
+    //     name: "name",
+    //     description: "description",
+    //     price: 0,
+    //     images: [""],
+    //     tags: ["men", "leisure", "shirt"]
+    // });
     
-}
+    return (
+        <FullHeightPage>
+            <Container>
+                {/* <Carousel>
+                    {
+                        product.images.map((image, index) => (
+                            <Carousel.Item key={index}>
+                                <Image
+                                    src={image}
+                                    layout="fill"
+                                />
+                            </Carousel.Item>
+                        )
+                    )}
+                </Carousel> */}
+                <Container className={styles.productInfoColumn}>
+                    <span className={styles.productName}>{product.name}</span>
+                    <span className={styles.productPrice}>${product.price}</span>
+                    <p className={styles.productDescription}>{product.description}</p>
+                    <div className={styles.productTags}>
+                        { product.tags.map((tag, index) => (
+                            <ProductTag key={index} tag={tag} />
+                        ))}
+                    </div>
+                </Container>
+            </Container>
+        </FullHeightPage>
+    )
+};
 
-const ProductDetails = () => {
- return (
-    <div>
-    </div>
- )
-}
-
-export default ProductDetails;
+export default ProductInfo;
