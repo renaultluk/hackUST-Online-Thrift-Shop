d78f3bc3aaf81793fe8d1083960e715e494a606a
Yoshi <38344465+yoshi-man@users.noreply.github.com>
2022-04-03 22:50:04 +0800

    added ordering functionality to shopping cart

--- main/pages/shopping-cart.js
+++ main/pages/shopping-cart.js
@@ -2,19 +2,21 @@ import { useEffect, useState } from "react";
 import { Container, Button, Row, Col } from "react-bootstrap";
 import Image from "next/image";
 
-import { collection, doc, getDocs, getDoc } from 'firebase/firestore';
+import { doc, getDoc, setDoc, Timestamp } from 'firebase/firestore';
 import { firestore } from "../utils/firebase";
+import { useAuth } from "../utils/AuthContext";
 
 import useShoppingStore from "../utils/ShoppingStore";
 import FullHeightPage from "../components/FullHeightPage";
 
 import styles from "../styles/shopping-cart.module.css";
+import generateOrderId from "../utils/utils";
 
 const ShoppingCart = () => {
     const shopStore = useShoppingStore();
+    const { loadingUser, currentUser } = useAuth();
+
     const [cart, setCart] = useState([]);
-    // const cart = []
-    // let total = 0;
     
     const fetchData = async () => {
         let i = 1;
@@ -33,6 +35,26 @@ const ShoppingCart = () => {
                 setCart(cart => [...cart, tmpItem]);
             }
         })
+    }
+
+    const handleCheckOutButton = () => {
+        /**
+         * For each checkout, we create an orderId linking a list of productId with embedded info and userId
+         */
+        const orderId = generateOrderId()
+        const order = {
+            orderId : orderId,
+            orderDate : Timestamp.now(),
+            userId : currentUser.uid,
+            totalSpending: cart.reduce((acc, item) => acc + item.price, 0),
+            productsBought : cart,
+        }
+
+        const orderRef = doc(firestore, 'orders', orderId);
+        setDoc(orderRef, order , { merge: true });
+
+        setCart([]);
+        shopStore.clearShoppingCart();
     }
 
     useEffect(() => {
@@ -81,9 +103,21 @@ const ShoppingCart = () => {
                         <h3>Total: {total}</h3>
                     </Col>
                     <Col xs lg={2}  className="d-flex justify-content-end">
-                        <Button
-                            variant="success"
-                        >Checkout</Button>
+                        {  
+                        !loadingUser&&currentUser&&
+                            <Button
+                                variant="success"
+                                onClick={handleCheckOutButton}
+                            >Checkout</Button>
+                        }
+
+                        {  
+                        !loadingUser&&!currentUser&&
+                            <Button
+                                href="/login"
+                                variant="success"
+                            >Login/Signup</Button>
+                        }
                     </Col>
 
                 </Row>
